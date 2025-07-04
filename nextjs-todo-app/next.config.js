160b45be4d850afb1b833f9cc789eee9807fb6bd
Koki Yamamoto <koki.yamamoto@system-exe.jp>
2025-07-04 14:52:56 +0900

    Fix basePath and assetPrefix for GitHub Pages deployment

--- nextjs-todo-app/next.config.js
+++ nextjs-todo-app/next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/koki-yamamoto-0702/nextjs-todo-app' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/koki-yamamoto-0702/nextjs-todo-app/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/koki-yamamoto-0702' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/koki-yamamoto-0702/' : '',
 }
 
 module.exports = nextConfig
